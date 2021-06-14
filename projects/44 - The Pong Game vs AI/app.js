fefc60cefe64cf1315dd83bcd44d3eab81d1f94f
Brian Bycynski <high5bri@hotmail.com>
2021-06-14 03:58:50 -0400

    Update app.js

--- projects/44 - The Pong Game vs AI/app.js
+++ projects/44 - The Pong Game vs AI/app.js
@@ -4,7 +4,7 @@
 const canvasEl = document.querySelector('canvas');
 const canvasContext = canvasEl.getContext('2d');
 
-canvasEl.width = 1500;
+canvasEl.width = 1400;
 canvasEl.height = 720;
 // --------------------------------------------------------------------
 let RIScore = new Audio();
